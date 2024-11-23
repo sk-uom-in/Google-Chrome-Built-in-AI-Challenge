f0616495d9851aa91298752992737e34b89fb3ab
Sambbhav khare <84086679+sk-uom-in@users.noreply.github.com>
2024-11-23 23:20:38 +0000

    "improved UX"

--- src/components/TranslationTool.jsx
+++ src/components/TranslationTool.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
 
+const CopyIcon = () => (
+  <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+    <path d="M20 9h-9a2 2 0 00-2 2v9a2 2 0 002 2h9a2 2 0 002-2v-9a2 2 0 00-2-2z"/>
+    <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1"/>
+  </svg>
+);
+
+const SelectIcon = () => (
+  <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+    <path d="M7 8h10M7 12h10M7 16h10"/>
+  </svg>
+);
+
 const TranslationTool = () => {
   const [inputText, setInputText] = useState('');
   const [translatedText, setTranslatedText] = useState('');
@@ -72,7 +85,7 @@ const TranslationTool = () => {
           onClick={getSelectedText}
           className="button primary"
         >
-          Get Selected Text
+          <SelectIcon /> Get Selected Text
         </button>
         <select 
           value={targetLanguage}
@@ -121,6 +134,12 @@ const TranslationTool = () => {
           <div className="translated-text">
             {translatedText}
           </div>
+          <button 
+            onClick={() => copyToClipboard(translatedText)}
+            className="button copy-button"
+          >
+            <CopyIcon /> Copy
+          </button>
         </div>
       )}
     </div>
